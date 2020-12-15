02dbd7e721c22a109a56e7a6ef8ea0e66473301b
JustJeo <j.cabrigas.writer@gmail.com>
2020-12-15 09:39:42 -0800

    finishes ALL seeder files; commits before undoing a seed migration and reseeding it

--- seeders/20201213232805-highscores.js
+++ seeders/20201213232805-highscores.js
@@ -40,6 +40,314 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       },
+      {
+        score: 100,
+        playerName: "YG Entertainment",
+        songId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 90,
+        playerName: "Jisoo",
+        songId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 80,
+        playerName: "Jennie",
+        songId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 70,
+        playerName: "Rose",
+        songId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 60,
+        playerName: "Lisa",
+        songId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 100,
+        playerName: "YG Entertainment",
+        songId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 90,
+        playerName: "Jisoo",
+        songId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 80,
+        playerName: "Jennie",
+        songId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 70,
+        playerName: "Rose",
+        songId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 60,
+        playerName: "Lisa",
+        songId: 5,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 100,
+        playerName: "YG Entertainment",
+        songId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 90,
+        playerName: "Jisoo",
+        songId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 80,
+        playerName: "Jennie",
+        songId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 70,
+        playerName: "Rose",
+        songId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 60,
+        playerName: "Lisa",
+        songId: 6,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 7,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 8,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 9,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 100,
+        playerName: "YG Entertainment",
+        songId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 90,
+        playerName: "Jisoo",
+        songId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 80,
+        playerName: "Jennie",
+        songId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 70,
+        playerName: "Rose",
+        songId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 60,
+        playerName: "Lisa",
+        songId: 10,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 11,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 12,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 13,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 14,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 100,
+        playerName: "YG Entertainment",
+        songId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 90,
+        playerName: "Jisoo",
+        songId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 80,
+        playerName: "Jennie",
+        songId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 70,
+        playerName: "Rose",
+        songId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 60,
+        playerName: "Lisa",
+        songId: 15,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 16,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 17,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 100,
+        playerName: "YG Entertainment",
+        songId: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 90,
+        playerName: "Jisoo",
+        songId: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 80,
+        playerName: "Jennie",
+        songId: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 70,
+        playerName: "Rose",
+        songId: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 60,
+        playerName: "Lisa",
+        songId: 18,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 19,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 20,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        score: 0,
+        playerName: "Coming Soon!",
+        songId: 21,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
     ])
   },
 
