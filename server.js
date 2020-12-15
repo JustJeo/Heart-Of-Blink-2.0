17cd8ac31fc4de2347c1121869ee52261b48c815
JustJeo <j.cabrigas.writer@gmail.com>
2020-12-15 14:27:38 -0800

    commits before unmigrating and remigrating

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const layouts = require("express-ejs-layouts");
 const methodOverride = require("method-override");
+const fs = require("fs")
 // const fs = require("fs")
 const app = express();
 
@@ -27,39 +28,47 @@ app.get("/", (req, res) => {
       .then(returnedAlbum => {
         console.log( "----------------Albums WORK!!!! -----------")
         res.render("home", {
-      albumName: returnedAlbum
+          albumName: returnedAlbum
     })
   })
 });
 
 // Get all songs
 app.get('/songs', (req, res) => {
-  db.song.findAll({
-    // where: {
-    //   albumId: 1
-    // }
-  }).then(returnedSongs => {
-    console.log("------------- SONGS PAGE!!! --------------")
-    db.album.findAll({
-      // { include: [db.song] }
-    }).then(returnedAlbum => {
-      res.render('songs', {
-        songs: returnedSongs,
-        album: returnedAlbum
+  db.song
+    .findAll()
+      // where: {
+      //   albumId: 1
+      // }
+      .then(returnedSongs => {
+        console.log("------------- SONGS PAGE!!! --------------")
+        db.album.findAll()
+          // { include: [db.song] }
+          .then(returnedAlbum => {
+            res.render('songs', {
+              songs: returnedSongs,
+              album: returnedAlbum
+            })
+          });
       })
-    });
-  })
 });
 
 // Get one game
 app.get('/game', (req, res) => {
+  // const gameData = JSON.parse(lyrics)
+  // // Can you parse over a seeder file?
+  // const gameIndex = parseInt(req.params.id)
   db.lyric
     .findAll()
       .then(returnedLyrics => {
-        console.log("--------- GET QUESTIONS!!!! -------------")
         console.log(returnedLyrics)
-        res.render('game', {
-      lyric: returnedLyrics
+        console.log("--------- GET QUESTIONS!!!! -------------")
+        db.song.findAll()
+        .then(returnedSong => {
+            res.render('game', {
+              lyrics: returnedLyrics,
+              song: returnedSong
+          })
     });
   })
 });
